b1f1ca3f250fe8ee45442247faff27e43bedbc8a
ivan-infanta-0 <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2021-10-24 23:26:17 +0800

    Added login and registration pages
    i barely remember how to bootstrap

--- django_app/main/urls.py
+++ django_app/main/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('buy.html', views.buy, name="buy"),
     path('reserve.html', views.reserve, name="reserve"),
-    path('borrow.html', views.borrow, name="borrow")
+    path('borrow.html', views.borrow, name="borrow"),
+
+    path('login.html', views.login, name="login"),
+    path('register.html', views.register, name="register"),
+    
 ]
