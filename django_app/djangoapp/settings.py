ae57d82c6afcc7fd4dab07bbdeed50eb41023755
ivan-infanta-0 <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2021-10-24 18:08:14 +0800

    Update settings.py

--- django_app/djangoapp/settings.py
+++ django_app/djangoapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,9 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 
 # Default primary key field type
