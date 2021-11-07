2470063d7cc14f454f72b2bd0f2eaa0f04991d61
ivan-infanta-0 <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2021-11-07 15:29:06 +0800

    Update models.py
    final field names are still not done

--- django_app/main/models.py
+++ django_app/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class ProgramTable(models.Model):
+    MUL_CHOICE = [
+        ('JAN', 'January'),
+        ('FEB', 'February'),
+        ('...', '...')
+    ]
+
+    # field_name = models.<fieldType>(*args, **kw)
+    name = models.CharField('field_name')
+
+    def __str__(self):
+        return '...'
+
+
+class ExtTable(models.Model):
+    field = models.ForeignKey(ProgramTable, on_delete=models.CASCADE)
+
+
+# manage.py makemigrations
+# manage.py migrate
