4dac96b74968d51882b0b7889739ddc4a7d48b81
ivan-infanta-0 <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2021-11-07 15:26:43 +0800

    Change database to mysql
    todo: change NAME field to something appropriate

--- django_app/djangoapp/settings.py
+++ django_app/djangoapp/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'djangoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 'django.db.backends.sqlite3',
+        'NAME': 'PLACEHOLDER_DATABASE_NAME',    # BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
