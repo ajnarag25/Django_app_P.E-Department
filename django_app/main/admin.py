39e10824eee8e3cbc94a2a531b0e1c0df78a3aa9
ivan-infanta-0 <ivanhayz.gonzales@gsfe.tupcavite.edu.ph>
2021-11-18 12:15:35 +0800

    Update admin.py

--- django_app/main/admin.py
+++ django_app/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Registration, Buy, Reserve, Borrow
+
 # Register your models here.
+admin.site.register(Registration)
+admin.site.register(Buy)
+admin.site.register(Reserve)
+admin.site.register(Borrow)
