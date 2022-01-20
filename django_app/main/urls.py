520c2fd931974a8fb2b61dca936a758d681b09ec
Aj Narag <ajnarag25@gmail.com>
2022-01-20 14:09:28 +0800

    buy, reserve, and buy crud successfully implemented

--- django_app/main/urls.py
+++ django_app/main/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('student/<student_id>/', views.student, name='student'),
     path('logout', views.logoutUser, name="logout"),
-    path('delete_student/<student_id>', views.delete_student, name="delete_student")
+    path('delete_student/<student_id>', views.delete_student, name="delete_student"),
+    path('editbuy/<buy_id>', views.editBuy, name="editbuy"),
+    path('deletebuy/<buy_id>', views.deletebuy, name="deletebuy"),
+    path('editreserve/<reserve_id>', views.editReserve, name="editreserve"),
+    path('deletereserve/<reserve_id>', views.deletereserve, name="deletereserve"),
+    path('editborrow/<borrow_id>', views.editBorrow, name="editborrow"),
+    path('deleteborrow/<borrow_id>', views.deleteborrow, name="deleteborrow")
 ]
